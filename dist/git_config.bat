ae575ba9a6a53d37b3f227859c9c863dcfc5895b
Yasmin Trembulack Agostinho <142899321+YasminTrembulack@users.noreply.github.com>
2024-09-05 12:36:51 -0300

    Delete dist/git_config.bat

--- dist/git_config.bat
+++
@@ -1,8 +0,0 @@
-cmdkey /delete:LegacyGeneric:target=git:https://github.com
-
-git config --global http.proxy http://disrct:etstech31415@rb-proxy-ca1.bosch.com:8080
-
-git config --global https.proxy https://disrct:etstech31415@rb-proxy-ca1.bosch.com:8080
-
-git config --global user.name "YasminTrembulack"
-git config --global user.email "yasmin.agostinho012@gmail.com"
